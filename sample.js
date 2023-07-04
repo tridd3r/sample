0d9b35d1e9882b86125801983595f2c72996f19d
krishna <krishna.nalam@outlook.in>
2023-07-05 00:54:59 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,32 +1,46 @@
-
-$(document).on('click', '.search', function () {
-    if ($(this).hasClass('inCheatSheet')) {
-        let value = $(this).prev().val();
-        value = value.toLowerCase();
+document.addEventListener('click', function (event) {
+    const target = event.target;
+    if (target.classList.contains('search')) {
+        const value = target.previousElementSibling.value.toLowerCase();
         console.log(value);
-        let refID = $('.cheatSheet-inner').data('ref');
-        $('.cheatModal .cheatSheet-inner').remove();
-        $.post('includes/calls/cheatSheet.php', ({ search: value, ref: refID }), function (data) {
-            $('.cheatModal').append(data);
-        })
-    } else {
-        let value = $(this).prev().val(), filter = '', status = '', page = '';
-        let search = '&search=' + value;
-        $('.filter select').each(function () {
-            if ($(this).data().type == 'filter') {
-                filter = '&filter=' + $(this).val();
+        if (target.classList.contains('inCheatSheet')) {
+            const refID = document.querySelector('.cheatSheet-inner').dataset.ref;
+            document.querySelector('.cheatModal .cheatSheet-inner').remove();
+            fetch('includes/calls/cheatSheet.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `search=${value}&ref=${refID}`
+            })
+            .then(response => response.text())
+            .then(data => {
+                document.querySelector('.cheatModal').insertAdjacentHTML('beforeend', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        } else {
+            let filter = '';
+            let status = '';
+            let page = '';
+            const search = `&search=${value}`;
+            document.querySelectorAll('.filter select').forEach(function (select) {
+                if (select.dataset.type === 'filter') {
+                    filter = `&filter=${select.value}`;
+                }
+                if (select.dataset.type === 'status') {
+                    const thisStatus = select.value === 'New Quote' ? 'New+Quote' : select.value;
+                    status = `&status=${thisStatus}`;
+                }
+            });
+            const urlParams = new URLSearchParams(window.location.search);
+            for (const [key, value] of urlParams.entries()) {
+                if (key === 'page') {
+                    page = page === '' ? `page=${value}` : page;
+                }
             }
-            if ($(this).data().type == 'status') {
-                let thisStatus = $(this).val() == "New Quote" ? 'New+Quote' : $(this).val();
-                status = '&status=' + thisStatus;
-            }
-        })
-        const urlParams = new URLSearchParams(window.location.search);
-        for (const [key, value] of urlParams.entries()) {
-            if (key == 'page') {
-                page = page == '' ? 'page=' + value : page;
-            }
+            window.location.assign(`dashboard.php?${page}${search}${filter}${status}`);
         }
-        window.location.assign(`dashboard.php?${page}${search}${filter}${status}`);
     }
-})
+});
